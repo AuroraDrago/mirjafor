a89b3195914bf333bead8626ae7c1478b3dfbcdb
Aurorabtw <56597339+Aurorabtw@users.noreply.github.com>
2023-04-08 07:55:44 +0600

    website test

---
+++ website/script.js
@@ -0,0 +1,8 @@
+// Get the button element by ID
+const button = document.getElementById("myButton");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+  // Navigate to Instagram page
+  window.location.href = "https://www.instagram.com/aurorabtw_/";
+});
